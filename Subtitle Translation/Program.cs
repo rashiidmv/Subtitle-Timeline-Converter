b3f64e39697fa8c1079700487ea76850fd60e757
Rashid  MV <rashiidmv@gmail.com>
2019-10-29 12:35:19 +0530

    Initial check in

---
+++ Subtitle Translation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Subtitle_Translation {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Give source file path having correct time line (May be english)");
+            Console.WriteLine("Give destination file path");
+
+            Console.WriteLine("Enter Soure file path");
+            String sourcePath = Console.ReadLine(); ;
+            while(!File.Exists(sourcePath)) {
+                Console.WriteLine("Please enter correct source file path");
+                sourcePath = Console.ReadLine(); ;
+                Console.Read();
+            }
+            string[] source = File.ReadAllLines(sourcePath);
+            string input = "N";
+            do {
+                Console.WriteLine("Enter Destination file path");
+                String destiPath = Console.ReadLine(); ;
+                while(!File.Exists(destiPath)) {
+                    Console.WriteLine("Please enter correct source file path");
+                    destiPath = Console.ReadLine();
+                    Console.Read();
+                }
+                string[] destination = File.ReadAllLines(destiPath);
+                for(int i = 0; i < source.Length; i += 3) {
+                    destination[i] = source[i];
+                }
+
+                File.WriteAllLines(destiPath, destination);
+                Console.WriteLine("Successfully created " + destiPath);
+                Console.WriteLine("Do you have another file for translation ?(Y?N)");
+                input = Console.ReadLine();
+
+            } while(input.ToUpper() == "Y");
+
+            Console.Read();
+        }
+    }
+}
